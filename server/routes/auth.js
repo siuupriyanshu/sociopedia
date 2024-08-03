95f2579ed4f3cf8149136ab9e3488328141711ac
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-03 19:19:29 +0545

    Backend Data Add and Demo

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,8 @@
-import express from 'express';
+import express from "express";
+import { login } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.post('/login', login);
+router.post("/login", login);
 
 export default router;
